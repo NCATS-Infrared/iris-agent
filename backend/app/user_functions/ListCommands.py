dc1128d4bd71bd0c02745e97b528e2c948bf9756
Stefano <srensi@stanford.edu>
2018-07-02 12:53:58 -0700

    Commands function working

--- backend/app/user_functions/ListCommands.py
+++ backend/app/user_functions/ListCommands.py
@@ -1,15 +1,17 @@
 from iris import state_types as t
 from iris import IrisCommand
-# from user_functions.SmartAPIQueries import ListAllTags
+from user_functions.SmartAPIQueries import *
 
 class ListCommands(IrisCommand):
 	title = "Show Commands"
 	examples = ["What commands are available?"]
 	def command(self):
-		# cmds = []
-		return "hey"
+		cmd_classes = [ListAllKnowledgeSources, ListAllTags, SearchKnowledgeSourceTitles, 
+		SearchKnowledgeSourceFull]
+		cmds = [i.title for i in cmd_classes]
+		return cmds
 	
 	def explanation(self, result):
-		return "Here are commands you can run " + str(result)
+		return "Here are commands you can run:\n" + '\n'.join(result)
 
 ListCommands = ListCommands()
