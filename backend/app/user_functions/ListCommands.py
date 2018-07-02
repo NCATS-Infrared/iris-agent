0d214528a38b0037abae1db262ffb1395417ab25
Stefano <srensi@stanford.edu>
2018-07-02 12:40:54 -0700

    2nd attempt at commands function

--- backend/app/user_functions/ListCommands.py
+++ backend/app/user_functions/ListCommands.py
@@ -1,15 +1,15 @@
 from iris import state_types as t
 from iris import IrisCommand
-from SmartAPIQueries import * 
+# from user_functions.SmartAPIQueries import ListAllTags
 
 class ListCommands(IrisCommand):
-    title = "COMMANDS"
-    examples = ["What commands are available?",
-    			"What can you do IRIS?",
-    			"What commands can I run?"]
-	argument_types = {}
+	title = "Show Commands"
+	examples = ["What commands are available?"]
 	def commands(self):
-		cmds = [ListAllTags.title, 
-				SearchKnowledgeSourceTitles.title,
-				SearchKnowledgeSourceFull.title]
-		return 
+		# cmds = []
+		return "hey"
+	
+	def explanation(self, result):
+		return ["Here are commands you can run ", str(result)]
+
+ListCommands = ListCommands()
