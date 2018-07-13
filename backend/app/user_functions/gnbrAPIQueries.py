9653e90e6749f26fced000c331aa14917e794437
Stefano <srensi@stanford.edu>
2018-07-12 18:28:37 -0700

    added gnbrAPIQueries file

---
+++ backend/app/user_functions/gnbrAPIQueries.py
@@ -0,0 +1,13 @@
+'''
+Commands to perform basic queries in SmartAPI
+'''
+
+
+from iris import state_types as t
+from iris import IrisCommand
+
+from iris import state_machine as sm
+from iris import util as util
+from iris import iris_objects
+from user_functions.API import gnbrAPI
+
